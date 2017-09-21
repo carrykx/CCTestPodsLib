be9adfc98aef1ca91cf63663c831c0162665cdbc
carrykx <carry.kx@foxmail.com>
2017-09-21 18:13:02 +0800

    初始化

---
+++ CCTestPodsLib/Classes/CCTestPodsLib.h
@@ -0,0 +1,13 @@
+//
+//  CCTestPodsLib.h
+//  CCTestPodsLib
+//
+//  Created by carry on 2017/9/21.
+//  Copyright © 2017年 dhyt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTestPodsLib : NSObject
+
+@end
---
+++ CCTestPodsLib/Classes/CCTestPodsLib.m
@@ -0,0 +1,13 @@
+//
+//  CCTestPodsLib.m
+//  CCTestPodsLib
+//
+//  Created by carry on 2017/9/21.
+//  Copyright © 2017年 dhyt. All rights reserved.
+//
+
+#import "CCTestPodsLib.h"
+
+@implementation CCTestPodsLib
+
+@end
